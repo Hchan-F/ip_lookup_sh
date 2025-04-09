dfaac5569eb7978a9474c06a183364edb4766975
Hchan-F <12345678+Hchan-F@users.noreply.github.com>
2025-04-09 21:13:48 +0200

    Initial commit - IP Lookup Script avec boucle utilisateur

---
+++ README.md
@@ -0,0 +1,20 @@
+# IP Lookup Script (Bash)
+
+Ce script Bash permet de :
+- Demander un nom de domaine à l’utilisateur
+- Résoudre l’adresse IP via la commande `dig`
+- Afficher l’IP et l’enregistrer dans un fichier `logs_ip.txt`
+- Boucler sur plusieurs noms de domaine selon l’entrée utilisateur
+
+## 💡 Objectif pédagogique
+
+Ce projet fait partie de mon apprentissage de Bash et de la logique réseau.
+
+## ✅ Améliorations prévues
+
+- Suppression de la duplication dans le `if`
+- Ajout d’une option pour afficher les logs
+- Gestion d’erreur si l’IP est vide
+- Interface interactive en ligne de commande (menu)
+
+## 📅 Prochain commit prévu : Revoir la logique de boucle (`break` vs variable booléenne) et suppression duplication `if` le **samedi 12 avril**
---
+++ ip_lookup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+continuer=true
+
+while [ "$continuer"=true ]; do
+    echo "Entrez un nom de domaine (ex : google.com) : "
+    read nom_domaine
+    ip=$(dig +short $nom_domaine | head -n 1)
+    echo "L'adresse IP est : $ip"
+    echo "$(date) : $nom_domaine ->  $ip" >> logs_ip.txt
+    echo "Informations enregistrées dans logs_ip.txt"
+    echo "Voulez-vous ajouter un autre nom de domaine (o/n)"
+    read reponse
+        if [ "$reponse" = "o" ]; then
+           echo "Entrez un nom de domaine : "
+           read nom_domaine
+           ip=$(dig +short $nom_domaine | head -n 1)
+           echo "L'adresse IP est : $ip"
+           echo "$(date) : $nom_domaine -> $ip" >> logs_ip.txt
+           echo "Informations enregistrées dans logs_ip.txt"
+        else
+           break
+       fi
+done
